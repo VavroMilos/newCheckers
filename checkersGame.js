d92ec7589523a51357e8680e954c130d45630a7b
ItamarCCohen <102979502+ItamarCCohen@users.noreply.github.com>
2022-05-01 21:27:09 +0300

    Add files via upload

---
+++ checkersGame.js
@@ -0,0 +1,16 @@
+
+
+
+class CheckersGame {
+    constructor(container) {
+        this.container = container
+        this.board = new Board();
+        this.board.appendTo(this.container)
+    }
+
+    clear() {
+        this.container.innerHTML = '';
+
+
+    }
+}
